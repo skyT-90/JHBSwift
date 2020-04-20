dcaf356d8743f49bc2a8dcc00b53afc92d04a9a3
jianghongbo <jianghongbo_kzx@citic.com>
2020-04-20 19:57:15 +0800

    navView

--- JHBSwift/Home/JHBHomeViewController.swift
+++ JHBSwift/Home/JHBHomeViewController.swift
@@ -12,10 +12,16 @@ class JHBHomeViewController: JHBBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        JHBNavView.self *navView = JHBNavView.init(frame: CGRect.init(x: 0, y: 100, width: KscreenWidth, height: 80))
         // Do any additional setup after loading the view.
+        view.addSubview(navView)
     }
     
+    fileprivate lazy var navView:JHBNavView = {
+        let navView = JHBNavView.init(frame: CGRect.init(x: 0, y: 100, width: KscreenWidth, height: 80))
+        navView.navTitle = "导航栏"
+        return navView
+    }()
 
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(true, animated: true)
--- JHBSwift/UtilsView/JHBNavView.swift
+++ JHBSwift/UtilsView/JHBNavView.swift
@@ -7,15 +7,68 @@
 //
 
 import UIKit
-
+import SnapKit
 class JHBNavView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var navTitle: String? {
+        didSet{
+            titleLabel.text = navTitle ?? "hahaha"
+        }
     }
-    */
 
+    fileprivate lazy var titleLabel:UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.backgroundColor = UIColor.red
+        titleLabel.font = UIFont.systemFont(ofSize: 20);
+        titleLabel.textColor = UIColor.white
+        titleLabel.textAlignment = NSTextAlignment.center
+        return titleLabel
+        
+    }()
+    
+    fileprivate lazy var backBtn: UIButton = {
+        let backBtn = UIButton()
+        backBtn.setImage(UIImage(named: "nav_btn_back_white"), for: .normal)
+        backBtn.sizeToFit()
+        backBtn.addTarget(self, action: #selector(backAction(bt:)), for: .touchUpInside)
+        return backBtn
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.blue
+        addChildView()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        backBtn.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(20)
+//            make.top.equalToSuperview().offset(35)
+            make.centerY.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints { (make) in
+//            make.left.equalTo(backBtn.snp_left).offset(10)
+            make.width.equalTo(300)
+            make.height.equalTo(44)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func backAction(bt:UIButton) -> Void {
+        NSLog("123")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
+
+extension JHBNavView {
+    fileprivate func addChildView(){
+        addSubview(titleLabel)
+        addSubview(backBtn)
+    }
+}
+
